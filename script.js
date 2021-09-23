8edbfa586a374864eaf42325980ad087699d422a
卫宇星 <weiyuxing@weiyuxingdeMacBook-Air.local>
2021-09-22 20:48:00 -0400

    ss

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ var Me = {
     name: "Yuxing Wei",
     age: 21,
     StudentID:"702519749",
+    talk:function(){alert("another... Sky... walk...");}
 };
+document.getElementById("Me").innerHTML = Me.name;
